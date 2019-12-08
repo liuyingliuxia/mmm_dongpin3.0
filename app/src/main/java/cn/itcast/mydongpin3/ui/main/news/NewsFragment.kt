e025e9b7562d9d83b736179a58d60a8b29335322
夏 小 <1652976019@qq.com>
2019-12-08 16:50:49 +0800

    完成 冻品商城 今日资讯 我的工作 在首页的展示

--- app/src/main/java/cn/itcast/mydongpin3/ui/main/news/NewsFragment.kt
+++ app/src/main/java/cn/itcast/mydongpin3/ui/main/news/NewsFragment.kt
@@ -6,20 +6,48 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.GridLayoutManager
+import cn.itcast.easy_recycler.MallAdapter
 import cn.itcast.mydongpin3.R
+import kotlinx.android.synthetic.main.fragment_frozen.view.*
+import kotlinx.android.synthetic.main.fragment_news.view.*
 
 
 /**
  * 今日资讯 碎片
  */
 class NewsFragment : Fragment() {
-
+    private var list: MutableList<String> = ArrayList()
+    private lateinit var adapter: NewsAdapter
+    private lateinit var gridLayoutManager: GridLayoutManager
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_news, container, false)
+        val mView =  inflater.inflate(R.layout.fragment_news, container,false)
+        list = getList()
+        adapter = NewsAdapter(list, context!!)
+        gridLayoutManager = GridLayoutManager(context,1)
+        gridLayoutManager.orientation = GridLayoutManager.VERTICAL
+        mView.rv_news.layoutManager = gridLayoutManager
+        mView.rv_news.adapter = adapter
+        adapter.setOnItemClick(object : NewsAdapter.OnItemClickListener {
+            override fun OnItemClick(view: View, position: Int) {
+                Toast.makeText(activity, position.plus(1).toString(), Toast.LENGTH_SHORT).show()
+            }
+        })
+        return mView
+
+    }
+
+
+    fun getList(): MutableList<String> {
+
+        for (i in 1..10) {
+            list.add(i.times(10).toString())
+        }
+        return list
     }
 
 
--- app/src/main/java/cn/itcast/mydongpin3/ui/main/work/WorkFragment.kt
+++ app/src/main/java/cn/itcast/mydongpin3/ui/main/work/WorkFragment.kt
@@ -1,12 +1,15 @@
 package cn.itcast.mydongpin3.ui.main.work
 
-
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.GridLayoutManager
 import cn.itcast.mydongpin3.R
+import cn.itcast.mydongpin3.ui.main.news.NewsAdapter
+import kotlinx.android.synthetic.main.fragment_news.view.*
 
 /**
  * 我的工作 碎片
@@ -17,8 +20,10 @@ class WorkFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_work, container, false)
+        val mView =  inflater.inflate(R.layout.fragment_work, container,false)
+
+        return mView
+
     }
 
 
